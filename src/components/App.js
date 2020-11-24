5ba322ec59bb04a7a7a3d064d0dd25bfcc4e018e
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-24 03:53:09 -0500

    Add YouTube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
 
 class App extends React.Component {
-    render() {
-        return (
-            <div className="ui container">
-                <SearchBar />
-            </div>
-        );
-    }
+  state = { videos: [] };
+
+  onTermSubmit = async term => {
+    const {
+      data: { items },
+    } = await youtube.get('/search', {
+      params: {
+        q: term,
+      },
+    });
+
+    this.setState({ videos: items });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <p>I have {this.state.videos.length} videos.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
