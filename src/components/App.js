c482f96e82961d0897917565a1ebe205a102d49b
Adam Carter <adam.clayton.carter@gmail.com>
2020-11-24 06:02:17 -0500

    Add VideoDetail component

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,59 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
+import Spinner from './Spinner';
 import youtube from '../apis/youtube';
 
 class App extends React.Component {
-  state = { videos: [] };
+  state = { videos: [], selectedVideo: null };
+
+  componentDidMount = () => {
+    this.onTermSubmit('buildings');
+  };
 
   onTermSubmit = async term => {
-    const {
-      data: { items },
-    } = await youtube.get('/search', {
+    const { data } = await youtube.get('/search', {
       params: {
         q: term,
       },
     });
 
-    this.setState({ videos: items });
+    this.setState({
+      videos: data.items,
+      selectedVideo: data.items[0],
+    });
+  };
+
+  onVideoSelect = selectedVideo => {
+    this.setState({ selectedVideo });
   };
 
   render() {
     return (
       <div className="ui container">
-        <SearchBar onFormSubmit={this.onTermSubmit} />
-        <p>I have {this.state.videos.length} videos.</p>
+        <div className="ui grid">
+          <div className="row">
+            <div className="sixteen wide column">
+              <SearchBar onFormSubmit={this.onTermSubmit} />
+            </div>
+          </div>
+          <div className="row">
+            <div className="eleven wide column">
+              {this.state.selectedVideo ? (
+                <VideoDetail video={this.state.selectedVideo} />
+              ) : (
+                <Spinner />
+              )}
+            </div>
+            <div className="five wide column">
+              <VideoList
+                videos={this.state.videos}
+                onVideoSelect={this.onVideoSelect}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
